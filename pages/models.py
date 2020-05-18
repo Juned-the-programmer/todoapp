b58beee9c9c9a661e02e79a68aff91267984ff12
juned_programmer <davdajuned7698@gmail.com>
2020-05-18 12:11:06 +0530

    firststep

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
 # Create your models here.
+class todo(models.Model):
+    action = models.CharField(max_length=100)
+    date_added = models.DateField(auto_now_add=True)
+
+class complete(models.Model):
+    actioncomplete = models.CharField(max_length=100)
+    date_completed = models.DateField(auto_now_add=True)
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def index(request):
-    return render(request,'pages/base.html')
+    if request.method=="POST":
+        name = request.POST['name']
+
+        Todo = todo (
+            action = name
+        )
+        Todo.save()
+    return render(request,'pages/index.html')
+
+def task(request):
+    tasks = todo.objects.all()
+    if request.method == "POST":
+        tasks.delete()
